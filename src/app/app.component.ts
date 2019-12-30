85196e50b6997d112900326936c568008b2a5063
marbifer <luisa.velasquez@ayi-asociados.com>
2019-12-30 15:35:09 +0200

    Add Sass and Bootstrap updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  template: `
-    <div>
-        <nav class='navbar navbar-default'>
-            <div class='container-fluid'>
-                <a class='navbar-brand'>{{pageTitle}}</a>
-                <ul class='nav navbar-nav'>
-                    <li><a [routerLink]="['/welcome']">Home</a></li>
-                    <li><a [routerLink]="['/movies']">Movies List</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class='container'>
-            <router-outlet></router-outlet>
-        </div>
-     </div>
-    `
+    selector: 'pm-root',
+    templateUrl: './app.component.html',
+    styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  pageTitle: string = 'Movies Application';
+    pageTitle: string = 'Movies Application';
 }
