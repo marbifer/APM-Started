4968077ae9b1462f90b7d0c01201a74b3c23a2d6
marbifer <35464670+marbifer@users.noreply.github.com>
2020-01-03 10:21:31 -0300

    added animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './app.animation';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 @Component({
     selector: 'pm-root',
     templateUrl: './app.component.html',
-    styleUrls: ["./app.component.scss"]
+    styleUrls: ["./app.component.scss"],
+    animations: [slideInAnimation]
 })
 export class AppComponent {
     pageTitle: string = 'Movies Application';
-
+    loading = true;
     public menu = {
         items: [
             {
@@ -20,4 +23,24 @@ export class AppComponent {
             }
         ]
     };
+
+    constructor(
+        private router: Router,
+       ) {
+        router.events.subscribe((routerEvent: Event) => {
+        this.checkRouterEvent(routerEvent);
+        });
+    }
+
+    checkRouterEvent(routerEvent: Event): void {
+        if (routerEvent instanceof NavigationStart) {
+          this.loading = true;
+        }
+    
+        if (routerEvent instanceof NavigationEnd ||
+            routerEvent instanceof NavigationCancel ||
+            routerEvent instanceof NavigationError) {
+          this.loading = false;
+        }
+      }
 }
