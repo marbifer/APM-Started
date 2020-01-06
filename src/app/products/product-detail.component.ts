ad2cbc3be97410b52b731e659c894254145a5e19
marbifer <35464670+marbifer@users.noreply.github.com>
2020-01-05 22:24:32 -0300

    improve code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
-import { Movie, MovieDetail } from "./product";
-import { ProductService } from "./product.service";
-import { Subscription } from "rxjs";
+import { MovieDetail } from "./product";
+
 import { Store, select } from "@ngrx/store";
 import { Observable } from "rxjs";
 import * as moviesActions from "./state/movies.actions";
@@ -13,56 +12,35 @@ import * as fromMovies from "./state/movies.reducer";
   templateUrl: "./product-detail.component.html",
   styleUrls: ["./product-detail.component.css"]
 })
-export class ProductDetailComponent implements OnInit, OnDestroy {
+export class ProductDetailComponent implements OnInit {
   public errorMessage: string;
   public movie: MovieDetail;
+  public hideImage = false;
   private searched: string;
 
-  movieDetail$: Observable<MovieDetail>;
-  testDetail;
-
-  private subs: Subscription;
+  public movieDetail$: Observable<MovieDetail>;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _productService: ProductService,
     private store: Store<any>
   ) {}
 
   ngOnInit() {
+    this.hideImage = false;
     const param = this._route.snapshot.paramMap.get("id");
     this.searched = this._route.snapshot.paramMap.get("searched");
-    console.log("this.searched: ", this.searched);
+
     if (param) {
-      //const id = +param;
       const id = param;
-      this.getMovieDetal(id);
       this.store.dispatch(new moviesActions.LoadMovieDetail({ search: id }));
     }
-
     this.movieDetail$ = this.store.pipe(select(fromMovies.getMovieDetail));
-    this.movieDetail$.subscribe(state => {
-      this.testDetail = state;
-
-      console.log("state Detail: ", this.testDetail);
-    });
   }
 
   public onBack(): void {
     this._router.navigate(["/movies-list"], {
       queryParams: { searched: this.searched }
     });
-  }
-
-  private getMovieDetal(id: string): void {
-    this.subs = this._productService.getMovieDetail(id).subscribe(
-      (movie: MovieDetail) => (this.movie = movie),
-      error => (this.errorMessage = <any>error)
-    );
-  }
-
-  ngOnDestroy() {
-    this.subs.unsubscribe;
   }
 }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 
-import { Movie, MoviesList } from "./product";
-import { ProductService } from "./product.service";
+import { MoviesList } from "./product";
 import { Router, ActivatedRoute, Params } from "@angular/router";
 import { Subscription } from "rxjs";
-import { Store, select  } from "@ngrx/store";
+import { Store, select } from "@ngrx/store";
 import { Observable } from "rxjs";
 import * as moviesActions from "./state/movies.actions";
 import * as fromMovies from "./state/movies.reducer";
-
 
 @Component({
   templateUrl: "./product-list.component.html",
@@ -17,14 +15,16 @@ import * as fromMovies from "./state/movies.reducer";
 export class ProductListComponent implements OnInit, OnDestroy {
   public pageTitle: string = "Movies List";
   public errorMessage: string;
-  public filteredMovies: Movie[];
+
   public loading = false;
 
-  movies$ : Observable<MoviesList>;
-  test:MoviesList;
+  public movies$: Observable<MoviesList>;
+  public moviesError$: Observable<any>;
+  public moviesLoading$: Observable<any>;
+  
 
   private _movieSearched: string;
-  private subs: Subscription;
+  private moviesSub: Subscription;
 
   get movieSearched(): string {
     return this._movieSearched;
@@ -33,11 +33,16 @@ export class ProductListComponent implements OnInit, OnDestroy {
     this._movieSearched = value;
     this.loading = true;
     this.errorMessage = null;
-    this.performSearch(this.movieSearched.toLocaleLowerCase());
+
+    this.store.dispatch(
+      new moviesActions.LoadMovies({
+        search: this.movieSearched.toLocaleLowerCase()
+      })
+    );
+   
   }
 
   constructor(
-    private _productService: ProductService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private store: Store<any>
@@ -47,12 +52,19 @@ export class ProductListComponent implements OnInit, OnDestroy {
     this.activeRouteInit();
 
     this.movies$ = this.store.pipe(select(fromMovies.getMovies));
+    this.moviesError$ = this.store.pipe(select(fromMovies.getError));
+    this.moviesLoading$ = this.store.pipe(select(fromMovies.getMoviesLoading));
 
-    this.movies$.subscribe(state => {
-     
-      this.test = state;
-
-      console.log('state: ', this.test)
+    this.moviesSub = this.movies$.subscribe((moviesData: MoviesList) => {
+      if(moviesData){
+        const queryParams: Params = { searched: this.movieSearched };
+        this.router.navigate([], {
+          relativeTo: this.activatedRoute,
+          queryParams: queryParams,
+          queryParamsHandling: "merge"
+        });
+      }
+      
     });
   }
 
@@ -64,36 +76,9 @@ export class ProductListComponent implements OnInit, OnDestroy {
     });
   }
 
-  private performSearch(dataSearched: string): void {
-    this.subs = this._productService.getFilmsData$(dataSearched).subscribe(
-      (moviesData: MoviesList) => {
-        console.log("ver", moviesData);
-        this.loading = false;
-        if (moviesData.Response === "False" && this.movieSearched) {
-          this.errorMessage = moviesData.Error;
-          this.filteredMovies = null;
-        } else if (moviesData.Response === "False" && !this.movieSearched) {
-          this.errorMessage = "Please search a movie";
-          this.filteredMovies = null;
-        } else {
-          this.errorMessage = null;
-          this.filteredMovies = moviesData.Search;
-          const queryParams: Params = { searched: this.movieSearched };
-          this.router.navigate([], {
-            relativeTo: this.activatedRoute,
-            queryParams: queryParams,
-            queryParamsHandling: "merge"
-          });
+  
 
-          this.store.dispatch(new moviesActions.LoadMovies({search: dataSearched}));
-
-        }
-      },
-      error => (this.errorMessage = <any>error)
-    );
-  }
-
-  ngOnDestroy(){
-    this.subs.unsubscribe();
+  ngOnDestroy() {
+    this.moviesSub.unsubscribe();
   }
 }
